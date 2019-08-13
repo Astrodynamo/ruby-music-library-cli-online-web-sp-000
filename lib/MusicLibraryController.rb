094d2105405ef5cc21404c25e13c8ae89d2ea03d
Amanda King <amapickell@gmail.com>
2019-08-13 19:20:09 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -14,6 +14,8 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     answer = ""
+    case answer
+    
     while answer != "exit"
       puts "What would you like to do?"
       answer = gets.strip
