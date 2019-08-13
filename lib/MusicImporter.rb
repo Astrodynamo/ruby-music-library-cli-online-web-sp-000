8d9b877c5205a5f6013eb53ead8821036dbbc6b1
Amanda King <amapickell@gmail.com>
2019-08-13 17:24:35 +0000

    Automatically backed up by Learn

---
+++ lib/MusicImporter.rb
@@ -0,0 +1,24 @@
+class MusicImporter
+  attr_reader :path
+  
+  def initialize (path)
+    @path = path
+  end
+  
+  def files
+    
+  end
+  
+  def self.import
+    
+  end
+  
+  def self.new_from_filename
+    
+  end
+  
+  def self.create_from_filename
+    
+  end
+  
+end
---
+++ lib/Song.rb
@@ -0,0 +1,49 @@
+class Song
+  attr_accessor :name
+  attr_reader :artist, :genre
+  
+  @@all = []
+  
+  def initialize (name, artist = nil, genre = nil)
+    @name = name
+    self.artist=(artist) if artist != nil
+    self.genre=(genre) if genre != nil
+  end
+  
+  def save
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def self.create (name)
+    song = self.new(name)
+    song.save
+    song
+  end
+  
+  def artist= (artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+  
+  def genre= (genre)
+    @genre = genre
+    genre.songs << self if !genre.songs.include?(self)
+  end
+  
+  def self.find_by_name (name)
+    all.find {|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name (name)
+    self.find_by_name(name) || self.create(name)
+  end
+  
+end
