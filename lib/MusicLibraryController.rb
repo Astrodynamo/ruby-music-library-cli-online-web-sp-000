cc5208b0182fdb0f8982f9b62434e7bcf9523dd9
Amanda King <amapickell@gmail.com>
2019-08-13 18:19:14 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -13,7 +13,10 @@ class MusicLibraryController
     puts "To list all of the songs of a particular genre, enter 'list genre'."
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
-    puts "What would you like to do?"
+    while answer != "exit"
+      puts "What would you like to do?"
+      answer = gets.strip
+    end
   end
   
 end
